cc4f1ff46969e0586f158b452f459220e8640bbd
ninesky00 <jiangzho@gmail.com>
2021-02-01 22:30:09 -0600

    add user model, controller, view and routes as well as a root welcome (blank line) Co-authored-by: Catherine Dean <69832134+catherinemdean15@users.noreply.github.com> Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new 
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      flash[:notice] = "You signed up successfully"
+      user.update_columns(:is_registered? => true)
+      redirect_to root_path
+    else 
+      flash.now[:errors] = user.errors.full_messages
+      render "new"
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  resources :users, only: [:new, :create, :destroy] do 
+    member { get 'dashboard'}
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'registration', to: 'users#new', as: 'registration'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
