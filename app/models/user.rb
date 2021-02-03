fdf2b46f41206cf8a4f22747ae0aa2a2fc1a0277
Catherine Dean <catherinemdean15@gmail.com>
2021-02-03 15:45:12 -0700

    add relationships between user and party

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
 
   has_many :buddies, dependent: :destroy, class_name: :Friend
   has_many :friends, through: :buddies
+
+  has_many :parties_users
+  has_many :parties, through: :parties_users
+
+  def hosted_parties
+    require 'pry'; binding.pry
+  end
+
+  def invitied_parties; end
 end
