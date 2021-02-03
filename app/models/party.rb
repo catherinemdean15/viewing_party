9498542c2b7c1abb143f9ee6fd05090447c90037
Catherine Dean <catherinemdean15@gmail.com>
2021-02-03 16:25:54 -0700

    add dependent: :destroy to has_many relationship

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,5 @@
 class Party < ApplicationRecord
   belongs_to :movie
-  has_many :parties_users
+  has_many :parties_users, dependent: :destroy
   has_many :users, through: :parties_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :buddies, dependent: :destroy, class_name: :Friend
   has_many :friends, through: :buddies
 
-  has_many :parties_users
+  has_many :parties_users, dependent: :destroy
   has_many :parties, through: :parties_users
 
   def hosted_parties
