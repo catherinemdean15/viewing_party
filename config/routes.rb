2c257f02fc5cbba8387270f027d70b344d99ff63
Catherine Dean <catherinemdean15@gmail.com>
2021-02-03 15:44:52 -0700

    create route for movies index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :users, only: [:create, :destroy] do 
-    member { get 'dashboard'}
+  resources :users, only: %i[create destroy] do
+    member { get 'dashboard' }
   end
 
   resources :sessions, only: [:create]
   get 'registration', to: 'users#new', as: 'registration'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :movies, only: [:index]
 end
