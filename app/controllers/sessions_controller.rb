93981d362b40218f33ebe596d54ab506dd45eaff
Cydnee Owens <cks410@yahoo.com>
2021-02-02 16:36:25 -0800

    Clean-up per rubocop errors; 43 to 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,19 @@
 class SessionsController < ApplicationController
-  def new
-  end
-  
+  def new; end
+
   def create
-    user = User.find_by_email(params[:email].downcase)
+    user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-    redirect_to root_path, notice: "You are logged in!"
+      redirect_to root_path, notice: 'You are logged in!'
     else
-      flash.now[:alert] = "Invalid login email, or password"
-      render "new"
+      flash.now[:alert] = 'Invalid login email, or password'
+      render 'new'
     end
   end
 
   def destroy
-    session[:user_id] = nil 
-    redirect_to root_path, notice: "You are logged out"
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'You are logged out'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,23 @@
 class UsersController < ApplicationController
-  def new 
+  def new
     @user = User.new
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      flash[:notice] = "You signed up successfully"
-      user.update_columns(:is_registered? => true)
+      flash[:notice] = 'You signed up successfully'
+      # user.update_columns(:is_registered? => true)
+      user.update(is_registered?: true)
       redirect_to root_path
-    else 
+    else
       flash.now[:errors] = user.errors.full_messages
-      render "new"
+      render 'new'
     end
   end
 
-  private 
+  private
+
   def user_params
     params.require(:user).permit(:email, :password)
   end
