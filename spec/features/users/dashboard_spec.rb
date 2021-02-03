8829c29ac92139da51e5070251cfe50a8227ffba
Catherine Dean <catherinemdean15@gmail.com>
2021-02-03 16:08:31 -0700

    create test for viewing parties section on user dashboard

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -7,12 +7,15 @@ RSpec.describe 'dashboard index' do
     @user.authenticate(@user.password)
     @friend = User.create!(email: 'friend1@email.com', password: 'password', is_registered?: true)
     Friend.create(user_id: @user.id, friend_id: @friend.id)
-    @party_1 = Party.create!(start_time: 'beginning time', end_time: 'ending_time')
-    @party_2 = Party.create!(start_time: 'beginning time', end_time: 'ending_time')
-    @movie_1 = @party_1.movies.create!(title: 'Mulan', run_time: '1 hour 12 min', genre: 'Family')
-    @movie_2 = @party_2.movies.create!(title: 'Oceans 11', run_time: '2 hours 10 min', genre: 'Action')
+
+    @movie_1 = Movie.create!(title: 'Mulan', run_time: '1 hour 12 min', genre: 'Family')
+    @movie_2 = Movie.create!(title: 'Oceans 11', run_time: '2 hours 10 min', genre: 'Action')
+    @party_1 = @movie_1.parties.create!(start_time: 'beginning time', end_time: 'ending_time')
+    @party_2 = @movie_2.parties.create!(start_time: 'beginning time', end_time: 'ending_time')
+
     PartiesUser.create!(party_id: @party_1.id, user_id: @user.id, host: true)
     PartiesUser.create!(party_id: @party_2.id, user_id: @user.id, host: false)
+
     visit dashboard_user_path(@user.id)
   end
 
