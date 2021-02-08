ebc7e642c9ee068dd2c586dfffa370db3d4f95dd
Catherine Dean <catherinemdean15@gmail.com>
2021-02-08 09:02:39 -0700

    add validations to party

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,6 +1,7 @@
 class Party < ApplicationRecord
+  validates :start_time, :date, :duration, presence: true
+
   belongs_to :movie
   has_many :parties_users, dependent: :destroy
   has_many :users, through: :parties_users
 end
-
