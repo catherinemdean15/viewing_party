9323bf8c5b04a9cd645b8a570e98685806da768c
Cydnee Owens <cks410@yahoo.com>
2021-02-09 14:17:25 -0800

    add sad path testing for new user

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -9,8 +9,10 @@ RSpec.describe "users new page" do
 
   it "can create a new user" do 
     visit registration_path
+
     user_email = "test5@gmail.com"
     user_password = "test5test5"
+
     fill_in "user[email]", with: user_email
     fill_in "user[password]", with: user_password
     fill_in "user[password_confirmation]", with: user_password
@@ -19,5 +21,17 @@ RSpec.describe "users new page" do
     user = User.find_by(email: user_email)
     expect(current_path).to eq(dashboard_user_path(user))
     expect(page).to have_content("You signed up successfully")
+  end
+
+  it "displays error message for missing information for a new user" do 
+    visit registration_path
+
+    user_email = "test5@gmail.com"
+    user_password = "test5test5"
+    
+    fill_in "user[email]", with: user_email
+    click_button 'Register'
+    save_and_open_page
+    expect(page).to have_content("Password can't be blank")
   end
 end
