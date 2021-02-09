986a1a1d7be190caf11a06ec0c2b539ece751833
Cydnee Owens <cks410@yahoo.com>
2021-02-09 15:12:03 -0800

    Reduce rubocop errors 26 to 6

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,19 +1,21 @@
 class MovieService
-  def self.call_moviedb(path, params = {})
-    response = connection.get(path) do |req|
-      req.params = params
-      req.params[:api_key] = ENV['movie_api_key']
+  class << self
+    def call_moviedb(path, params = {})
+      response = connection.get(path) do |req|
+        req.params = params
+        req.params[:api_key] = ENV['movie_api_key']
+      end
+      parse_data(response)
     end
-    parse_data(response)
-  end
 
-  private
+    private
 
-  def self.connection
-    Faraday.new('https://api.themoviedb.org') 
-  end
+    def connection
+      Faraday.new('https://api.themoviedb.org')
+    end
 
-  def self.parse_data(data)
-    JSON.parse(data.body, symbolize_names: true)
+    def parse_data(data)
+      JSON.parse(data.body, symbolize_names: true)
+    end
   end
 end
