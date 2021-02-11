5efecc13c43d85fa37a8975149db1532739555c4
Catherine Dean <catherinemdean15@gmail.com>
2021-02-10 20:51:10 -0700

    Store google password in application.yml (blank line) Co-authored-by: Joe Jiang <69106714+ninesky00@users.noreply.github.com> Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-address:              'smtp.gmail.com',
-port:                 587,
-domain:               'gmail.com',
-user_name:            'joejviewingparty@gmail.com',
-password:             'Viewparty2010!',
-authentication:       'plain',
-enable_starttls_auto: true  
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: 'joejviewingparty@gmail.com',
+  password: ENV['google_password'],
+  authentication: 'plain',
+  enable_starttls_auto: true
 }
